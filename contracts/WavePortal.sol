255b42ad4f6184da330f4433e6dc8f2787bcc941
Babatunde Yakub <tundenasri@gmail.com>
2021-09-18 01:24:34 +0100

    save all waves in an array

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,28 +5,34 @@ import "hardhat/console.sol";
 
 contract WavePortal {
   uint totalWaves;
-  struct Waver {
-    address addr;
-    uint waved_at;
-  }
-  Waver[] wavers;
+  event newWave(address indexed from, uint timestamp, string message);
 
-  mapping(address => Waver) public hmm;
+  struct Wave {
+    address waver;
+    string message;
+    uint timestamp;
+  }
+
+  Wave[] waves;
 
   constructor() {
     console.log('Lets gooooo. My first contract');
   }
 
-  function wave() public {
+  function wave(string memory _message) public {
     totalWaves += 1;
-    Waver memory waver = Waver(msg.sender, block.timestamp);
-    wavers.push(waver);
-    hmm[msg.sender] = waver;
-    console.log("%s waved", msg.sender);
+    console.log("%s waved", msg.sender, _message);
+    Wave memory waver = Wave(msg.sender, _message, block.timestamp);
+    waves.push(waver);
+    emit newWave(msg.sender, block.timestamp, _message);
   }
 
   function getTotalWaves() view public returns (uint) {
     console.log('We have %d total waves', totalWaves);
     return totalWaves;
+  }
+
+  function getWaves() view public returns (Wave[] memory) {
+    return waves;
   }
 }
